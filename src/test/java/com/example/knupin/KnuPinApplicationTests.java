83675bf5cbdc191785c1f18021bf8669810d6c5d
yerim10044001 <rim0399@naver.com>
2023-02-10 16:01:09 +0900

    Initial commit

---
+++ src/test/java/com/example/knupin/KnuPinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuPinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
